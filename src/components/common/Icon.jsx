8dd4b11334249e8160f6a45a0f559637e7a2745c
Monique Huynh <47400590+mhxynh@users.noreply.github.com>
2026-04-02 20:12:47 -0400

    Icon clean up

---
+++ src/components/common/Icon.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Icon = ({
+  name,
+  category = '',
+  size = 'sm',
+  className = '',
+  color = '#96151D',
+  ...props
+}) => {
+  const sizeMap = {
+    xs: 12,
+    sm: 16,
+    md: 20,
+    lg: 24,
+    xl: 32,
+  };
+
+  const dimension = sizeMap[size] || sizeMap.md;
+
+  return (
+    <svg
+      width={dimension}
+      height={dimension}
+      stroke={color}
+      fill="none"
+      className={`ui-icon icon-${category}-${name}`}
+      aria-hidden="true"
+      {...props}
+    >
+      <use xlinkHref={`icons.svg#icon-${category}-${name}`} />
+    </svg>
+  );
+};
+
+Icon.propTypes = {
+  name: PropTypes.string.isRequired,
+  category: PropTypes.oneOf(['actions', 'deco', 'status', 'nav']),
+  size: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
+  color: PropTypes.string,
+  className: PropTypes.string,
+};
+
+export default Icon;
